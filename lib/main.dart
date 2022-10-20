140a2f3f55001efb1e68dbf8fea0051c6aa853c6
abhi9044 <abhishek111204cse@gmail.com>
2022-10-20 13:00:03 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Permission.camera.request();
+  await Permission.microphone.request();
+  await Permission.storage.request();
+  await Permission.manageExternalStorage.request();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: InAppWebView(
+        onWebViewCreated: (controller) {
+          controller.addJavaScriptHandler(
+              handlerName: 'myHandlerName',
+              callback: (args) {
+                print("Yolo Swag");
+                print(args[0]['avatarUrl']);
+              });
+        },
+        initialUrlRequest:
+            URLRequest(url: Uri.parse("https://www.insane.ai/test-page")),
+      )), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
